5fd80776dee4a30368eab438b520ef7a44d766a2
Serhii Minin <sergey.minin@brainberry.ua>
2018-07-26 09:48:51 +0300

    add google login

--- src/root/index.js
+++ src/root/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { credentials } from '../api';
 
-const Root = () => (
-  <div className="root">
-    Test
-  </div>
-);
+const Root = () => {
+  const { CLIENT_ID } = credentials;
+
+  return (
+    <div className="root">
+      <GoogleLogin
+        clientId={CLIENT_ID}
+        buttonText="Login"
+        onSuccess={res => console.log(res)}
+        onFailure={e => console.log(e)}
+      />
+    </div>
+  );
+};
 
 export default Root;
