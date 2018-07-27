79d8753f182befe1b95afa68e9f48b47c1d5fe3c
Serhii Minin <sergey.minin@brainberry.ua>
2018-07-27 15:25:56 +0300

    add wrapper container component

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import { WordsList } from '../../components/words-list';
 import { WordsListItem } from '../../components/words-list-item';
+import { BlocksContainer } from '../../components/blocks-container';
 import { api } from '../../api/fetcher';
 
 class Main extends Component {
   state = {
     words: [],
   };
+
   componentDidMount() {
     api.getWordsList()
-      .then(words => this.setState({ words }))
+      .then(words => this.setState({ words }));
   }
+
   render() {
     const { words } = this.state;
     return (
-      <WordsList>
-        {words.map(word => (
-          <WordsListItem word={word} key={word._id}/>
-        ))}
-      </WordsList>
-    )
+      <BlocksContainer>
+        <WordsList>
+          {words.map(word => (
+            <WordsListItem word={word} key={word._id}/>
+          ))}
+        </WordsList>
+      </BlocksContainer>
+    );
   }
 }
 
