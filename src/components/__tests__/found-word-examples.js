a3c41d401469551761df91f48393315a78a4897e
Serhii Minin <sergey.minin@brainberry.ua>
2018-08-22 16:52:38 +0300

    add tests for found word examples

---
+++ src/components/__tests__/found-word-examples.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import FoundWordExample from '../found-word-examples/component';
+
+describe('Clickable Word', () => {
+
+  test('Empty found word', () => {
+    const wrapper = shallow(
+      <FoundWordExample
+        foundWord={{}}
+        pushTextToInput={() => {}}
+      />
+    );
+
+    expect(wrapper.find('div').at(0).text()).toEqual('No results');
+  });
+
+  test('Received examples', () => {
+    const wrapper = shallow(
+      <FoundWordExample
+        foundWord={{
+          examples: [{ example: 'example', id: '1'}]
+        }}
+        pushTextToInput={() => {}}
+      />
+    );
+
+    expect(wrapper.find('ListOfClickableStrings')).toHaveLength(1);
+  });
+
+});
