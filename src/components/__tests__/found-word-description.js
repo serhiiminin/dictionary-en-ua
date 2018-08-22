92ec1ecc454163465387f60ce4358fba9bf01a43
Serhii Minin <sergey.minin@brainberry.ua>
2018-08-22 15:45:48 +0300

    add tests for found word description

---
+++ src/components/__tests__/found-word-description.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import FoundWordDescription from '../found-word-description/component';
+
+describe('Clickable Word', () => {
+
+  test('Empty found word', () => {
+    const wrapper = shallow(
+      <FoundWordDescription
+        foundWord={{}}
+        pushTextToInput={() => {}}
+      />
+    );
+
+    expect(wrapper.closest('div').text()).toEqual('Here will be detailed description');
+  });
+
+  test('Received just translation', () => {
+    const wrapper = shallow(
+      <FoundWordDescription
+        foundWord={{
+          en: 'en',
+          ru: 'ru',
+        }}
+        pushTextToInput={() => {}}
+      />
+    );
+
+    expect(wrapper.find('Jss(LineExplanation)')).toHaveLength(7);
+    expect(wrapper
+      .findWhere(node => node.props().label === 'English')
+      .contains('en')
+    ).toBeTruthy();
+    expect(wrapper
+      .findWhere(node => node.props().label === 'Russian')
+      .contains('ru')
+    ).toBeTruthy();
+  });
+
+  test('Received part of speech', () => {
+    const wrapper = shallow(
+      <FoundWordDescription
+        foundWord={{
+          partOfSpeech: ['noun']
+        }}
+        pushTextToInput={() => {}}
+      />
+    );
+
+    expect(wrapper.contains('noun')).toBeTruthy();
+  });
+});
