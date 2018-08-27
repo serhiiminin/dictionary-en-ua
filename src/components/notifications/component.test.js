7527176d1df18e33f8b92330b5d6c40250f24221
Serhii Minin <sergey.minin@brainberry.ua>
2018-08-27 10:13:22 +0300

    add tests for notification items

---
+++ src/components/notifications/component.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Notifications from './component';
+
+describe('Notifications', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <Notifications notifications={[]}>
+        children
+      </Notifications>
+    );
+  });
+  test('render empty', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('render with one item', () => {
+    wrapper.setProps({
+      notifications: [{
+        id: 'id',
+        text: 'text',
+        type: 'success',
+      }]
+    });
+    expect(wrapper).toMatchSnapshot();
+  })
+});
