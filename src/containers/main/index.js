684d3c7d6f0719ac2882d0370258ec61d0c7e598
Serhii Minin <sergey.minin@brainberry.ua>
2018-09-19 16:40:05 +0300

    use yarn instead of npm

--- src/containers/main/index.js
+++ src/containers/main/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import trident from '../../images/trident.svg';
 
 const MainContainer = () => (
-  <div>Main page</div>
+  <div>
+    <img src={trident} alt="trident"/>
+    <h3>Учітесь, читайте, І чужому научайтесь, Й свого не цурайтесь</h3>
+  </div>
 );
 
 export default MainContainer;
