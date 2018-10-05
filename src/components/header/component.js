6a3468c05924091d45c66931145b6f8708a1bdda
Serhii Minin <sergey.minin@brainberry.ua>
2018-10-05 17:26:52 +0300

    try styles components with header

--- src/components/header/component.js
+++ src/components/header/component.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { HeaderNavigation } from '..';
+import stylesVariables from '../../constants/styles-variables';
 import routes from '../../routes';
 
-const Header = ({ classes }) => (
-  <div className={classes.header}>
+const HeaderBlock = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  padding: ${stylesVariables.padding.medium} 0;
+`;
+
+const StyledRouterLink = styled(Link)`
+  color: ${stylesVariables.colors.text};
+  text-decoration: none;
+  font-size: 1.5rem;
+`;
+
+const Header = () => (
+  <HeaderBlock>
     <h1>
-      <Link
-        to={routes.root}
-        className={classes.headerLink}
-      >The dictionary</Link>
+      <StyledRouterLink to={routes.root}>The dictionary</StyledRouterLink>
     </h1>
     <HeaderNavigation/>
-  </div>
+  </HeaderBlock>
 );
-
-Header.propTypes = {
-  classes: PropTypes.objectOf(PropTypes.string),
-};
-
-Header.defaultProps = {
-  classes: {},
-};
 
 export default Header;
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,3 @@
-import injectSheet from 'react-jss';
-import { compose } from 'recompose';
-import styles from './styles';
 import Header from './component';
 
-const enhance = compose(
-  injectSheet(styles),
-);
-
-export default enhance(Header);
+export default Header;
