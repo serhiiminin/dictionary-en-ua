0c13ac23533203e3ae1987a19d3ca5f51824a99c
Serhii Minin <sergey.minin@brainberry.ua>
2018-10-09 09:49:40 +0300

    add styles for chip

---
+++ src/components-mui/chip/component.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Chip } from '@material-ui/core';
+
+const ChipCustomized = ({ classes, ...restProps }) => (
+  <Chip
+    classes={{
+      root: classes.root,
+      disabled: classes.disabled,
+    }}
+    {...restProps}
+  />
+);
+
+ChipCustomized.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string),
+};
+
+ChipCustomized.defaultProps = {
+  classes: {},
+};
+
+export default ChipCustomized;
