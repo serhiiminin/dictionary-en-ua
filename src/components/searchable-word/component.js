fd6f43e55bf98f64e431d9535dd1620369253218
Serhii Minin <sergey.minin@brainberry.ua>
2018-10-11 16:20:38 +0300

    delimiter for searchable words

--- src/components/searchable-word/component.js
+++ src/components/searchable-word/component.js
@@ -3,22 +3,27 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import routes from '../../routes';
 
-const SearchableWord = ({ word, classes }) => (
-  <Link
-    to={`${routes.words.search}?query=${word}`}
-    className={classes.clickableWord}
-  >
-    {word}
-  </Link>
+const SearchableWord = ({ word, classes, delimiter }) => (
+  <React.Fragment>
+    <Link
+      to={`${routes.words.search}?query=${word}`}
+      className={classes.clickableWord}
+    >
+      {word}
+    </Link>
+    {delimiter}
+  </React.Fragment>
 );
 
 SearchableWord.propTypes = {
   word: PropTypes.string.isRequired,
   classes: PropTypes.objectOf(PropTypes.string),
+  delimiter: PropTypes.node,
 };
 
 SearchableWord.defaultProps = {
   classes: {},
+  delimiter: null,
 };
 
 export default SearchableWord;
