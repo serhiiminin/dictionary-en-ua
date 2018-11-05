2525707caacceed4ce14b45838a1b6fe871a2f72
Serhii Minin <sergey.minin@brainberry.ua>
2018-11-05 10:39:02 +0200

    change default color of button

--- src/containers/words-edit/index.js
+++ src/containers/words-edit/index.js
@@ -2,10 +2,12 @@ import { withRouter } from "react-router-dom";
 import { compose } from "recompose";
 import { withWords } from "../../context/words";
 import WordEdit from "./container";
+import { withLoadingNames } from "../../context/loading-names";
 
 const enhance = compose(
   withRouter,
-  withWords
+  withWords,
+  withLoadingNames,
 );
 
 export default enhance(WordEdit);
