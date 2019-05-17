3484f83f433d658a739651d8e77a3d2e6f7af24d
Serhii Minin <minin.serhii@gmail.com>
2019-05-17 11:18:24 +0300

    use hooks where it is possible

--- src/pages/main/component.js
+++ src/pages/main/component.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Slide } from '@material-ui/core';
 import ReactRouterPropTypes from 'react-router-prop-types';
 import { joinUrl, mergeSearch } from 'url-joiner';
@@ -6,65 +6,49 @@ import { ButtonSearch } from '../../components';
 import routes from '../../routes';
 import SC from './styles';
 
-class MainContainer extends Component {
-  static propTypes = {
-    history: ReactRouterPropTypes.history.isRequired,
-  };
+const MainContainer = ({ history }) => {
+  const [searchValue, setSearchValue] = useState('');
 
-  state = {
-    searchValue: '',
-  };
+  const handleOnChange = event => setSearchValue(event.target.value);
 
-  handleOnChange = event => {
-    const { value } = event.target;
+  const handleOnSearch = () => history.push(joinUrl(routes.words.search, mergeSearch({ query: searchValue })));
 
-    this.setState({ searchValue: value });
-  };
-
-  handleOnSearch = () => {
-    const { searchValue } = this.state;
-    const { history } = this.props;
-
-    history.push(joinUrl(routes.words.search, mergeSearch({ query: searchValue })));
-  };
-
-  handleOnEnterPress = event => {
+  const handleOnEnterPress = event => {
     const key = event.key || event.keyCode;
 
     if (key === 'Enter' || key === 13) {
-      this.handleOnSearch();
+      handleOnSearch();
     }
   };
 
-  render() {
-    const { searchValue } = this.state;
-    const isEmpty = !searchValue;
+  return (
+    <SC.MainPage>
+      <Slide in direction="right">
+        <SC.SearchBlockWrapper>
+          <SC.PageTitle>
+            The Best Dictionary ever.
+            <SC.BoldText>TRY IT!</SC.BoldText>
+          </SC.PageTitle>
+          <SC.SearchBlock>
+            <SC.TextField
+              label="your word"
+              value={searchValue}
+              onChange={handleOnChange}
+              onKeyUp={handleOnEnterPress}
+              variant="outlined"
+            />
+            <ButtonSearch onClick={handleOnSearch} disabled={!searchValue} variant="contained" color="primary">
+              search
+            </ButtonSearch>
+          </SC.SearchBlock>
+        </SC.SearchBlockWrapper>
+      </Slide>
+    </SC.MainPage>
+  );
+};
 
-    return (
-      <SC.MainPage>
-        <Slide in direction="right">
-          <SC.SearchBlockWrapper>
-            <SC.PageTitle>
-              The Best Dictionary ever.
-              <SC.BoldText>TRY IT!</SC.BoldText>
-            </SC.PageTitle>
-            <SC.SearchBlock>
-              <SC.TextField
-                label="your word"
-                value={searchValue}
-                onChange={this.handleOnChange}
-                onKeyUp={this.handleOnEnterPress}
-                variant="outlined"
-              />
-              <ButtonSearch onClick={this.handleOnSearch} disabled={isEmpty} variant="contained" color="primary">
-                search
-              </ButtonSearch>
-            </SC.SearchBlock>
-          </SC.SearchBlockWrapper>
-        </Slide>
-      </SC.MainPage>
-    );
-  }
-}
+MainContainer.propTypes = {
+  history: ReactRouterPropTypes.history.isRequired,
+};
 
 export default MainContainer;
