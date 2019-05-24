e28b7711d3e393c94514613ec045871eb5b2f456
Serhii Minin <minin.serhii@gmail.com>
2019-05-24 15:56:13 +0300

    hide sign-up forms when authorized

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import LogOutContainer from './log-out';
-import SignUpForms from './sign-up-forms';
+import SignUpFormsRoute from './sign-up-forms';
 import routes from '../../routes';
 
 const AuthPage = () => (
   <Switch>
     <Route path={routes.auth.logOut} component={LogOutContainer} />
-    <Route path={routes.auth.root} component={SignUpForms} />
+    <SignUpFormsRoute path={routes.auth.root} />
   </Switch>
 );
 
--- src/pages/auth/sign-up-forms/index.js
+++ src/pages/auth/sign-up-forms/index.js
@@ -1,58 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import ReactRouterPropTypes from 'react-router-prop-types';
-import { Fade } from '@material-ui/core';
-import { Switch, Route, withRouter } from 'react-router-dom';
-import { LogInSuggestion, SignUpSuggestion, LogInForm, SignUpForm, ForgotPasswordForm } from '../../../chunks';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+import SignUpFormsCmp from './component';
 import routes from '../../../routes';
-import SC from './styles';
+import { withAuth } from '../../../context/auth';
 
-const SLIDING_TIME = 500;
+const SignUpFormsRoute = ({ isLoggedIn, ...rest }) => (
+  <Route
+    {...rest}
+    render={params =>
+      isLoggedIn ? (
+        <Redirect
+          to={{
+            pathname: routes.words.list,
+            redirectedFrom: params.location,
+          }}
+        />
+      ) : (
+        <SignUpFormsCmp {...params} />
+      )
+    }
+  />
+);
 
-const SignUpForms = ({ location }) => {
-  const [isSliding, setIsSliding] = useState(false);
-
-  useEffect(() => {
-    setIsSliding(true);
-    setTimeout(() => {
-      setIsSliding(false);
-    }, SLIDING_TIME);
-  }, [location.pathname]);
-
-  const isLeftActive = [routes.auth.logIn].includes(location.pathname);
-  const isRightActive = [routes.auth.signUp, routes.auth.forgotPassword].includes(location.pathname);
-
-  return (
-    <SC.Outer>
-      <SC.Background isLeft={isLeftActive} transitionDelay={SLIDING_TIME} />
-      <SC.Circle transitionDelay={SLIDING_TIME} isLeft={isLeftActive} />
-      <SC.HalfPart isActive={isLeftActive}>
-        {!isSliding && (
-          <Fade in={!isSliding}>
-            <Switch>
-              <Route path={routes.auth.logIn} component={LogInForm} />
-              <Route path={routes.auth.signUp} component={LogInSuggestion} />
-              <Route path={routes.auth.forgotPassword} component={LogInSuggestion} />
-            </Switch>
-          </Fade>
-        )}
-      </SC.HalfPart>
-      <SC.HalfPart isActive={isRightActive}>
-        {!isSliding && (
-          <Fade in={!isSliding}>
-            <Switch>
-              <Route path={routes.auth.logIn} component={SignUpSuggestion} />
-              <Route path={routes.auth.signUp} component={SignUpForm} />
-              <Route path={routes.auth.forgotPassword} component={ForgotPasswordForm} />
-            </Switch>
-          </Fade>
-        )}
-      </SC.HalfPart>
-    </SC.Outer>
-  );
+SignUpFormsRoute.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  render: PropTypes.func,
 };
 
-SignUpForms.propTypes = {
-  location: ReactRouterPropTypes.location.isRequired,
+SignUpFormsRoute.defaultProps = {
+  render: null,
 };
 
-export default withRouter(SignUpForms);
+export default withAuth(SignUpFormsRoute);
