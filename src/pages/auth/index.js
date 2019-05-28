ed40508d54b26dd9bfcd9524caacb606e5145fc8
Serhii Minin <minin.serhii@gmail.com>
2019-05-28 09:40:54 +0300

    use privat route for log out page

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Switch, Route, withRouter } from 'react-router-dom';
+import { Switch, withRouter } from 'react-router-dom';
+import { PrivateRoute } from '../../components';
 import LogOutContainer from './log-out';
 import SignUpFormsRoute from './sign-up-forms';
 import routes from '../../routes';
 
 const AuthPage = () => (
   <Switch>
-    <Route path={routes.auth.logOut} component={LogOutContainer} />
+    <PrivateRoute path={routes.auth.logOut} component={LogOutContainer} />
     <SignUpFormsRoute path={routes.auth.root} />
   </Switch>
 );
