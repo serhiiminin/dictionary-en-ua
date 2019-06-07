6524158c39851b2dcb36641fa32ebd4d4bfb5773
Serhii Minin <minin.serhii@gmail.com>
2019-06-07 09:56:30 +0300

    add a separate route for all pages related towords

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,4 @@
 export { default as AuthPage } from './auth';
 export { default as MainPage } from './main';
 export { default as PageNotFoundPage } from './not-found';
-export { default as WordsAddPage } from './word/add';
-export { default as WordsEditPage } from './word/edit';
-export { default as WordsListPage } from './word/list';
-export { default as WordPreviewPage } from './word/preview';
-export { default as WordSearchPage } from './word/search';
+export { default as WordPage } from './word';
---
+++ src/pages/word/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const WordsWrapper = styled.div`
+  margin-top: 7rem;
+`;
+
+export default { WordsWrapper };
