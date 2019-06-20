3af8bc14f3e2b634f40d58ba8a48b34a63e001e6
Serhii Minin <minin.serhii@gmail.com>
2019-06-20 15:31:16 +0300

    show preview data for a word

--- src/pages/word/preview/component.js
+++ src/pages/word/preview/component.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ReactRouterPropTypes from 'react-router-prop-types';
 
-const WordPreviewContainer = () => <div>Preview</div>;
+class WordPreviewContainer extends Component {
+  static propTypes = {
+    match: ReactRouterPropTypes.match.isRequired,
+    handleFetchWord: PropTypes.func.isRequired,
+    wordItem: PropTypes.shape({
+      word: PropTypes.string,
+      transcription: PropTypes.string,
+      gif: PropTypes.string,
+    }),
+  };
+
+  static defaultProps = {
+    wordItem: null,
+  };
+
+  componentDidMount() {
+    const { match, handleFetchWord } = this.props;
+    const { id } = match.params;
+
+    handleFetchWord(id);
+  }
+
+  render() {
+    const { wordItem } = this.props;
+    const { word, transcription, gif } = wordItem;
+
+    return (
+      <div>
+        <h1>{word}</h1>
+        <p>[{transcription}]</p>
+        <img src={gif} alt={word} />
+      </div>
+    );
+  }
+}
 
 export default WordPreviewContainer;
