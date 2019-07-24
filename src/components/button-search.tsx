26bd48baa003a5e93b961af2e9c83096808bfdf2
Serhii Minin <minin.serhii@gmail.com>
2019-07-24 13:49:37 +0300

    fix:  search button appearance

--- src/components/button-search.tsx
+++ src/components/button-search.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 import styled from 'styled-components';
 
-const ButtonSearch = styled((props): JSX.Element => <Button {...props} />)`
+const ButtonSearch = styled((props): JSX.Element => <Button {...props} classes={{ disabled: 'disabled' }} />)`
   && {
     background: linear-gradient(149.02deg, #86d1ff -11.95%, #bbd6fd 89.7%, #c5d7fd 89.71%, #c5d7fd 89.73%);
     box-shadow: 9px 9px 15px rgba(15, 99, 203, 0.179551);
@@ -14,6 +14,10 @@ const ButtonSearch = styled((props): JSX.Element => <Button {...props} />)`
     color: #fff;
     &:hover {
       background: linear-gradient(149.02deg, #86d1ff -11.95%, #bbd6fd 89.7%, #c5d7fd 89.71%, #c5d7fd 89.73%);
+    }
+
+    &.disabled {
+      box-shadow: 9px 9px 15px rgba(15, 99, 203, 0.179551);
     }
   }
 `;
