510379f1771c32aa48dcfdc5a09c0c8e6bcbb143
Serhii Minin <minin.serhii@gmail.com>
2019-07-25 13:58:33 +0300

    feat:  add ability to delete a word

--- src/pages/word/list.tsx
+++ src/pages/word/list.tsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import { compose } from 'recompose';
-import { LinearProgress } from '@material-ui/core';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
 import { withLoading, LI } from '../../context/loading';
 import { withWords, WI } from '../../context/words';
 import LN from '../../constants/loading-names';
-import { TitleBlock, WordListItem } from '../../components';
+import { TitleBlock, WordListItem, WordList } from '../../components';
 
 type Props = LI & WI & RouteComponentProps;
 
@@ -19,25 +18,19 @@ class WordsList extends Component<Props, {}> {
   }
 
   public render(): JSX.Element {
-    const { wordsList, checkIsLoading } = this.props;
+    const { wordsList, checkIsLoading, handleDeleteWord } = this.props;
     const isLoading = checkIsLoading(LN.words.list);
 
-    const Words =
-      wordsList.length === 0 ? (
-        "You don't have saved words at the moment"
-      ) : (
-        <div>
-          {wordsList.map(
-            (word): JSX.Element => (
-              <WordListItem key={word._id} word={word} />
-            )
-          )}
-        </div>
-      );
     return (
       <>
         <TitleBlock>Your words</TitleBlock>
-        {isLoading ? <LinearProgress /> : Words}
+        <WordList isLoading={isLoading}>
+          {wordsList.map(
+            (word): JSX.Element => (
+              <WordListItem key={word._id} word={word} onDelete={handleDeleteWord} />
+            )
+          )}
+        </WordList>
       </>
     );
   }
