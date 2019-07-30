c9c735f071807408bfe30eb54b2e7753feacb895
Serhii Minin <minin.serhii@gmail.com>
2019-07-30 16:38:07 +0300

    feat:  show words list panel

--- src/components/title-block.tsx
+++ src/components/title-block.tsx
@@ -1,12 +1,19 @@
 import styled from 'styled-components';
 import { ThemeProps } from '../types';
 
+interface OwnProps {
+  textAlign?: 'left' | 'center' | 'right';
+}
+
+type Props = OwnProps & ThemeProps;
+
 const TitleBlock = styled.h2`
   font-style: normal;
-  font-family: ${(props: ThemeProps): string => props.theme.main.fontFamily.cairoBold};
-  font-size: ${(props: ThemeProps): string => props.theme.main.fontSize.xl};
-  letter-spacing: ${(props: ThemeProps): string => props.theme.main.letterSpacing.xs};
-  color: ${(props: ThemeProps): string => props.theme.main.color.text};
+  font-family: ${(props: Props): string => props.theme.main.fontFamily.cairoBold};
+  font-size: ${(props: Props): string => props.theme.main.fontSize.xl};
+  letter-spacing: ${(props: Props): string => props.theme.main.letterSpacing.xs};
+  color: ${(props: Props): string => props.theme.main.color.text};
+  text-align: ${(props: Props): string => props.textAlign || 'left'};
   text-transform: uppercase;
   margin: 0;
 `;
