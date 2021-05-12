3a2f7bfb5051f7529f6f963e79d8cc8ebce31a15
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-12 12:23:14 +0000

    Deploy serhiiminin/dictionary to github.com/serhiiminin/dictionary.git:gh-pages

---
+++ precache-manifest.1680a96a5ef5e5fe82caa739fcd90001.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f0ba1f0a424b87ad46ca68a705d806",
+    "url": "/dictionary/index.html"
+  },
+  {
+    "revision": "c346213836abc6210219",
+    "url": "/dictionary/static/js/2.dcc8e79e.chunk.js"
+  },
+  {
+    "revision": "1d21179010ebae813b3b",
+    "url": "/dictionary/static/js/main.cc0f79c8.chunk.js"
+  },
+  {
+    "revision": "5ba4beaafaa44016f725",
+    "url": "/dictionary/static/js/runtime-main.4149271b.js"
+  },
+  {
+    "revision": "392ac39c08c9a2214bedda0a42546b94",
+    "url": "/dictionary/static/media/cairo-bold.392ac39c.eot"
+  },
+  {
+    "revision": "3abac7e429981a99d3f68a6a5f22ab06",
+    "url": "/dictionary/static/media/cairo-bold.3abac7e4.woff"
+  },
+  {
+    "revision": "4603362015236eac3be29482b2e975fa",
+    "url": "/dictionary/static/media/cairo-bold.46033620.woff2"
+  },
+  {
+    "revision": "e80068d0f326ec9a6054b3d401a7c16b",
+    "url": "/dictionary/static/media/cairo-bold.e80068d0.svg"
+  },
+  {
+    "revision": "f0be50317caf42a2d61b4efc1ec68183",
+    "url": "/dictionary/static/media/cairo-bold.f0be5031.ttf"
+  },
+  {
+    "revision": "307c27f9f9c7119b55e88f4f6095784a",
+    "url": "/dictionary/static/media/cairo-regular.307c27f9.eot"
+  },
+  {
+    "revision": "61fa42069eeb7663bd67f4f8cff282ed",
+    "url": "/dictionary/static/media/cairo-regular.61fa4206.ttf"
+  },
+  {
+    "revision": "b24d081bf792858b993a0ede176d75b7",
+    "url": "/dictionary/static/media/cairo-regular.b24d081b.woff"
+  },
+  {
+    "revision": "ba102829fabcb8d5b2e8b5599570a2e7",
+    "url": "/dictionary/static/media/cairo-regular.ba102829.woff2"
+  },
+  {
+    "revision": "c6f9cfc6f7147ba540ed5c022bd4cc97",
+    "url": "/dictionary/static/media/cairo-regular.c6f9cfc6.svg"
+  },
+  {
+    "revision": "ab310ed6da657fe2b1d69fefe6f90cbd",
+    "url": "/dictionary/static/media/crying-girl.ab310ed6.svg"
+  },
+  {
+    "revision": "bd58af2d30c20a111d634352f56ee0d3",
+    "url": "/dictionary/static/media/logo.bd58af2d.svg"
+  }
+]);
